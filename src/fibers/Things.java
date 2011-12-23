785f0a2881a95c1844ab42ebd907fd0485942651
MultiTool <JCAtkeson@gmail.com>
2011-12-23 14:01:15 -0500

    adding rotoplane

--- src/fibers/Things.java
+++ src/fibers/Things.java
@@ -247,8 +247,8 @@ public class Things {
       norm.Unitize();
     }
   }
-  /* *************************************************************************************************** */
 
+  /* *************************************************************************************************** */
   public static class CPoint extends PointNd {/* Control Point */
     /* *************************************************************************************************** */
 
@@ -309,6 +309,250 @@ public class Things {
       CPoints = new CPoint_List(0);
     }
     /* *************************************************************************************************** */
+    Roto_Plane planeform;
+
+    public static class Roto_Plane extends PlaneNd {
+      // the purpose of this class is to represent a sigmoid plane, to fit it to points, and to fit points to it.
+
+      private PointNd pingvec;
+      public double rangemin, rangectr, rangemax;
+      PointNd running_avg = new PointNd(ndims);
+      public double above, below;
+      PointNd normal = new PointNd(ndims_init);
+      PointNd desire = new PointNd(ndims_init);
+
+      /* *************************************************************************************************** */
+      public Roto_Plane(int num_dims) {
+        super(num_dims);
+        pingvec = new PointNd(ndims);
+        rangemin = -1.0;
+        rangectr = 0.0;
+        rangemax = 1.0;
+        above = 0.0;
+        below = 0.0;
+      }
+      /* *************************************************************************************************** */
+
+      public double ActFun(double xin) {
+        double OutVal;
+        if (false) {
+          return xin;
+        } else {
+          OutVal = xin / Math.sqrt(1.0 + xin * xin);/* symmetrical sigmoid function in range -1.0 to 1.0. */
+          return OutVal;
+        }
+        /*
+         * double power = 2.0;
+        OutVal = xin / Math.pow(1 + Math.abs(Math.pow(xin, power)), 1.0 / power);
+         */
+      }
+      /* *************************************************************************************************** */
+
+      public double Reverse_ActFun(double xin) {
+        double OutVal;// from http://www.quickmath.com/webMathematica3/quickmath/page.jsp?s1=equations&s2=solve&s3=basic
+        // reverse, inverse of sigmoid is:
+        OutVal = xin / (Math.sqrt(Math.abs(xin * xin - 1.0)));
+        return OutVal;
+      }
+      /* *************************************************************************************************** */
+
+      public double sigmoid_deriv(double Value) { /* modified for default return  -dbr */
+        /* Given the unit's activation value and sum of weighted inputs,
+         *  compute the derivative of the activation with respect to the sum.
+         *  Defined types are SIGMOID (-1 to +1) and ASYMSIGMOID (0 to +1).
+         * */
+        //Value *= (1.17 * 1.01);
+        Value *= (1.17 * 1.0095);// is 1.181115
+        Value = (Value + 1.0) / 2;
+//    Value = (Value * 0.5)+0.5;
+        double SigmoidPrimeOffset = 0.1;
+        /*  asymmetrical sigmoid function in range 0.0 to 1.0.  */
+        double returnval = (SigmoidPrimeOffset + (Value * (1.0 - Value)));
+        returnval *= 2.857142857142857;// modify to fit 1:1 slope of sigmooid
+        return returnval * 1.0;
+        //return ((SigmoidPrimeOffset + (Value * (1.0 - Value))));
+      }
+      /* *************************************************************************************************** */
+
+      public void Ping(PointNd pnt) {
+        //sigmoid_deriv
+        double phgt = pnt.loc[ninputs];
+        // first get the height of the plane at this loc,
+        double result;
+        result = this.Get_Height(pnt);
+        result = this.ActFun(result);
+        // corrector is the distance from the sigmoid plane TOWARD this point's height
+        double corr = phgt - result;
+
+        double bell = this.sigmoid_deriv(phgt);
+        corr *= bell;
+
+        // now create the recognition vector, based on this pnt's position, and 1.0 * this plane's height offset dimension
+        for (int cnt = 0; cnt < ninputs; cnt++) {
+          pingvec.loc[cnt] = pnt.loc[cnt];
+        }
+        pingvec.loc[ninputs] = 1.0;
+        Train_Inlinks(pingvec, ndims, 0.01, corr);
+
+        double pdrop = (phgt * 0.1);
+        this.rangectr = (this.rangectr * 0.9) + pdrop;
+        if (true) {
+          this.rangemax = ((this.rangemax - this.rangectr) * 0.9) + this.rangectr;
+          this.rangemin = ((this.rangemin - this.rangectr) * 0.9) + this.rangectr;
+          if (phgt < this.rangemin) {
+            this.rangemin = phgt;
+          }
+          if (phgt > this.rangemax) {
+            this.rangemax = phgt;
+          }
+        }
+        /* keep an approximate centroid for the cloud of points that hit me */
+        for (int cnt = 0; cnt < ninputs; cnt++) {
+          running_avg.loc[cnt] *= 0.9;
+          running_avg.loc[cnt] += 0.1 * pnt.loc[cnt];
+        }
+        above *= 0.9;
+        below *= 0.9;
+        if (pnt.loc[ninputs] > 0.0) {
+          above += 0.1;
+        } else if (pnt.loc[ninputs] < 0.0) {
+          below += 0.1;
+        }
+        //this.Balance();
+      }
+      /* *************************************************************************************************** */
+
+      public double Get_Sigmoid_Height(PointNd pnt) {
+        double hgt = this.Get_Height(pnt);
+        hgt = this.ActFun(hgt);
+        return hgt;
+      }
+      /* *************************************************************************************************** */
+
+      public void Balance() {
+        double sub_ratio = above;// ratio of above-zero to total
+        if (sub_ratio < 0.25 || 0.75 < sub_ratio) {
+          double midval = this.Get_Sigmoid_Height(running_avg);
+          double corr = 0.0 - midval;
+          // whoops, need to make average be like pingvec, for the training
+          running_avg.loc[ninputs] = 1.0;
+          Train_Inlinks(running_avg, ndims, 0.1, corr);// was lrate of 0.01
+        }
+        if (false) {// another experiment, try to flatten the plane.
+          for (int cnt = 0; cnt < ninputs; cnt++) {
+            if (Math.abs(this.loc[cnt]) > 0.7) {
+              this.loc[cnt] *= 0.999;
+            }
+          }
+        }
+      }
+      /* *************************************************************************************************** */
+
+      public void Attract_Point(PointNd pnt, PointNd pdesire) {
+        double shadow_hgt = this.Get_Height(pnt);// height on raw plane at this point's position.
+        double sigmoid_shadow_hgt = this.ActFun(shadow_hgt);// height on sigmoid plane at this point's position.
+        //double sigmoid_shadow_hgt = this.Get_Sigmoid_Height(pnt);// height on sigmoid plane at this point's position.
+
+        //sigmoid_shadow_hgt = shadow_hgt;
+        double delta_hgt = sigmoid_shadow_hgt - pnt.loc[ninputs];// distance from this point to sigmoid plane.
+
+        PointNd nrm = new PointNd(ndims);
+        this.Plane_Ramp_To_Normal(nrm);// get normal to raw plane.
+        nrm.Unitize();
+        PointNd delta = new PointNd(ndims);// delta is the vector from the point to its place on the (sigmoid) plane
+        delta.loc[ninputs] = delta_hgt;
+        double corrlen = delta.Dot_Product(nrm);// project delta onto normal, to get straight distance to plane.
+        delta.Copy_From(nrm);// multiply unit normal by corrector length to get correction vector.
+        delta.Multiply(corrlen);
+        pdesire.Copy_From(delta);
+        if (false) {// flee the centroid
+          delta.Copy_From(pnt);
+          delta.Subtract(running_avg);
+          if (delta.Magnitude(ninputs) <= 0.7) {
+            delta.Multiply(0.01);
+            delta.loc[ninputs] = 0.0;
+            pdesire.Add(delta);
+          }
+        }
+        if (false) {// flee the crack
+          PointNd steep = new PointNd(ndims);
+          normal.Get_Steepest(steep);
+          steep.loc[ninputs] = 0;
+          steep.Unitize();
+          double sgn = Math.signum(pnt.loc[ninputs]);
+          steep.Multiply(sgn);
+          steep.Multiply(0.001);
+          pdesire.Add(steep);
+        }
+        double range = Math.abs(this.rangemax - this.rangemin);
+        double unitrange = (2.0 - range) / 2.0;// is the range as 0 to 1.0
+        double finalrange = (0.9 * unitrange) + 0.1;//  this way the flattest ranges have the strongest self-inspired vertical attraction.
+
+        double vfactor = finalrange;
+        vfactor = 0.8;// works better with 2 layers
+        vfactor = 0.09;// works better with 7 layers, and maybe 3
+        //vfactor = 0.07;// test with 7
+        vfactor *= 0.3;
+        pdesire.loc[ninputs] *= vfactor;
+
+        double jitamp = 0.0001;
+        pdesire.Jitter(-jitamp, jitamp);
+        //pdesire.loc[ninputs] = 0.0;
+      }
+      /* *************************************************************************************************** */
+
+      public void Train_Inlinks(PointNd invec, int ninputs_local, double lrate, double corrector) {
+        double invec_squared = invec.Magnitude_Squared(ninputs_local);
+        if (invec_squared == 0.0) {
+          invec_squared = fudge;
+        }
+        for (int cnt = 0; cnt < ninputs_local; cnt++) {
+          double adj = (invec.loc[cnt] / invec_squared);// unitary adjustment tool
+          adj = adj * corrector * lrate;
+          this.loc[cnt] += adj;
+        }
+      }/* Train_Inlinks */
+      /* *************************************************************************************************** */
+
+
+      public void Plot_Gradient(Graphics2D g2) {
+        /* all about the gradient for display */
+        // , PointNd grad0, PointNd grad1
+        double hgt = this.loc[0];
+        double grad_x0;
+        double grad_y0;
+        double grad_x1, grad_y1;
+        PointNd steepest = new PointNd(ndims);
+        normal.Get_Steepest(steepest);
+        double[] ratios = new double[ninputs];// the ratios are NOT the inverse slopes.  they are from the steepest line.
+        for (int cnt = 0; cnt < ninputs; cnt++) {
+          ratios[cnt] = steepest.loc[cnt] / steepest.loc[ninputs];// inverse slope for each shadow of the steepest
+        }
+        if (true) {
+          double offset = planeform.loc[ninputs];
+          //offset *= Bounds.Rad(ninputs);
+          double height0 = -1.0 - offset;
+          double height1 = 1.0 - offset;
+          double brad = Bounds.Rad(ninputs);
+          //gradx0 = (int) (brad * (height0 * ratios[0])); grad_y0 = (int) (brad * (height0 * ratios[1])); grad_x1 = (int) (brad * (height1 * ratios[0])); grad_y1 = (int) (brad * (height1 * ratios[1]));
+          height0 *= brad;
+          height1 *= brad;
+          grad_x0 = (int) ((height0 * ratios[0]));
+          grad_y0 = (int) ((height0 * ratios[1]));
+          grad_x1 = (int) ((height1 * ratios[0]));
+          grad_y1 = (int) ((height1 * ratios[1]));
+        }
+        Color startColor = new Color(0.0f, 0.0f, 1.0f);//Color startColor = Color.blue;
+        Color endColor = new Color(1.0f, 0.0f, 0.0f);//Color endColor = Color.red;
+
+        GradientPaint gradient;
+        gradient = new GradientPaint(xorg + (int) grad_x0, yorg + (int) grad_y0, startColor, xorg + (int) grad_x1, yorg + (int) grad_y1, endColor);// A non-cyclic gradient
+        g2.setPaint(gradient);
+        g2.fillRect(xorg + (int) Bounds.minmax[0][0], yorg + (int) Bounds.minmax[0][1], (int) Bounds.Wdt(), (int) Bounds.Hgt());
+        g2.setColor(Color.white);
+        g2.drawLine(xorg + (int) grad_x0, yorg + (int) grad_y0, xorg + (int) grad_x1, yorg + (int) grad_y1);
+      }
+    }
 
     public void Draw_Me(Graphics2D gr) {
       for (CPoint cpnt : CPoints) {
@@ -450,7 +694,7 @@ public class Things {
       return (minmax[1][dim] + minmax[0][dim]) / 2.0;
     }
   }
-    /* *************************************************************************************************** */
+  /* *************************************************************************************************** */
   /* 
   is there any harm in only applying correctors to *one* of the nodes in the output layer?
   we could also just make the one last node.  probably easiest.
@@ -463,5 +707,4 @@ public class Things {
    * 
    * 
    */
-
 }
